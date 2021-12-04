2052b13e12036c2bab4add6d823a339018ace8ae
DanteZZ <dantezzstudio@gmail.com>
2021-12-04 17:43:07 +0700

    Обновил параметры

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-12-04T10:39:41.648Z",
+    "time": "2021-12-04T10:42:43.040Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
